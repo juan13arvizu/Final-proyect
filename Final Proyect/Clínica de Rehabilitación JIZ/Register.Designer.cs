a67a7b9561bcd845dc1d7887a1c001c64642000e
juan13arvizu <71351101+juan13arvizu@users.noreply.github.com>
2020-11-19 21:17:21 -0600

    Add files via upload

---
+++ Final Proyect/Clínica de Rehabilitación JIZ/Register.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace Clínica_de_Rehabilitación_JIZ
+{
+    partial class Register
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Register));
+            this.LogoLabel = new System.Windows.Forms.Label();
+            this.PswBoxtxt = new System.Windows.Forms.TextBox();
+            this.UserupdaterBotton = new System.Windows.Forms.Button();
+            this.PswBoxlabel = new System.Windows.Forms.Label();
+            this.UserBoxtxt = new System.Windows.Forms.TextBox();
+            this.UserBoxLabel = new System.Windows.Forms.Label();
+            this.Doblabel = new System.Windows.Forms.Label();
+            this.Procedurelabel = new System.Windows.Forms.Label();
+            this.DoBBoxtxt = new System.Windows.Forms.TextBox();
+            this.e = new System.Windows.Forms.Label();
+            this.ProcedureBoxtxt = new System.Windows.Forms.TextBox();
+            this.AddressBoxtxt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // LogoLabel
+            // 
+            this.LogoLabel.AutoSize = true;
+            this.LogoLabel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.LogoLabel.Location = new System.Drawing.Point(6, 5);
+            this.LogoLabel.Name = "LogoLabel";
+            this.LogoLabel.Size = new System.Drawing.Size(378, 30);
+            this.LogoLabel.TabIndex = 33;
+            this.LogoLabel.Text = "Clínica de Rehabilitación JIZ";
+            // 
+            // PswBoxtxt
+            // 
+            this.PswBoxtxt.BackColor = System.Drawing.Color.SkyBlue;
+            this.PswBoxtxt.Location = new System.Drawing.Point(11, 144);
+            this.PswBoxtxt.Name = "PswBoxtxt";
+            this.PswBoxtxt.Size = new System.Drawing.Size(192, 34);
+            this.PswBoxtxt.TabIndex = 32;
+            this.PswBoxtxt.TextChanged += new System.EventHandler(this.PswBoxtxt_TextChanged);
+            // 
+            // UserupdaterBotton
+            // 
+            this.UserupdaterBotton.BackColor = System.Drawing.Color.Fuchsia;
+            this.UserupdaterBotton.ForeColor = System.Drawing.Color.Transparent;
+            this.UserupdaterBotton.Location = new System.Drawing.Point(564, 141);
+            this.UserupdaterBotton.Name = "UserupdaterBotton";
+            this.UserupdaterBotton.Size = new System.Drawing.Size(137, 37);
+            this.UserupdaterBotton.TabIndex = 31;
+            this.UserupdaterBotton.Tag = "";
+            this.UserupdaterBotton.Text = "SAVE";
+            this.UserupdaterBotton.UseVisualStyleBackColor = false;
+            this.UserupdaterBotton.Click += new System.EventHandler(this.UserupdaterBotton_Click);
+            // 
+            // PswBoxlabel
+            // 
+            this.PswBoxlabel.AutoSize = true;
+            this.PswBoxlabel.BackColor = System.Drawing.Color.Blue;
+            this.PswBoxlabel.ForeColor = System.Drawing.Color.PowderBlue;
+            this.PswBoxlabel.Location = new System.Drawing.Point(6, 111);
+            this.PswBoxlabel.Name = "PswBoxlabel";
+            this.PswBoxlabel.Size = new System.Drawing.Size(139, 30);
+            this.PswBoxlabel.TabIndex = 30;
+            this.PswBoxlabel.Text = "Password";
+            // 
+            // UserBoxtxt
+            // 
+            this.UserBoxtxt.BackColor = System.Drawing.Color.Aquamarine;
+            this.UserBoxtxt.Location = new System.Drawing.Point(2, 74);
+            this.UserBoxtxt.Name = "UserBoxtxt";
+            this.UserBoxtxt.Size = new System.Drawing.Size(201, 34);
+            this.UserBoxtxt.TabIndex = 29;
+            this.UserBoxtxt.TextChanged += new System.EventHandler(this.UserBoxtxt_TextChanged);
+            // 
+            // UserBoxLabel
+            // 
+            this.UserBoxLabel.AutoSize = true;
+            this.UserBoxLabel.BackColor = System.Drawing.Color.Blue;
+            this.UserBoxLabel.ForeColor = System.Drawing.Color.PaleTurquoise;
+            this.UserBoxLabel.Location = new System.Drawing.Point(6, 41);
+            this.UserBoxLabel.Name = "UserBoxLabel";
+            this.UserBoxLabel.Size = new System.Drawing.Size(145, 30);
+            this.UserBoxLabel.TabIndex = 28;
+            this.UserBoxLabel.Text = "Username";
+            this.UserBoxLabel.Click += new System.EventHandler(this.UserBoxLabel_Click);
+            // 
+            // Doblabel
+            // 
+            this.Doblabel.AutoSize = true;
+            this.Doblabel.BackColor = System.Drawing.Color.Blue;
+            this.Doblabel.ForeColor = System.Drawing.Color.PowderBlue;
+            this.Doblabel.Location = new System.Drawing.Point(209, 111);
+            this.Doblabel.Name = "Doblabel";
+            this.Doblabel.Size = new System.Drawing.Size(173, 30);
+            this.Doblabel.TabIndex = 35;
+            this.Doblabel.Text = "Day of Birth";
+            // 
+            // Procedurelabel
+            // 
+            this.Procedurelabel.AutoSize = true;
+            this.Procedurelabel.BackColor = System.Drawing.Color.Blue;
+            this.Procedurelabel.ForeColor = System.Drawing.Color.PowderBlue;
+            this.Procedurelabel.Location = new System.Drawing.Point(516, 45);
+            this.Procedurelabel.Name = "Procedurelabel";
+            this.Procedurelabel.Size = new System.Drawing.Size(152, 30);
+            this.Procedurelabel.TabIndex = 36;
+            this.Procedurelabel.Text = "Procedure";
+            // 
+            // DoBBoxtxt
+            // 
+            this.DoBBoxtxt.BackColor = System.Drawing.Color.SkyBlue;
+            this.DoBBoxtxt.Location = new System.Drawing.Point(209, 144);
+            this.DoBBoxtxt.Name = "DoBBoxtxt";
+            this.DoBBoxtxt.Size = new System.Drawing.Size(301, 34);
+            this.DoBBoxtxt.TabIndex = 38;
+            this.DoBBoxtxt.TextChanged += new System.EventHandler(this.DoBBoxtxt_TextChanged);
+            // 
+            // e
+            // 
+            this.e.AutoSize = true;
+            this.e.BackColor = System.Drawing.Color.Blue;
+            this.e.ForeColor = System.Drawing.Color.PowderBlue;
+            this.e.Location = new System.Drawing.Point(218, 41);
+            this.e.Name = "e";
+            this.e.Size = new System.Drawing.Size(123, 30);
+            this.e.TabIndex = 39;
+            this.e.Text = "Address";
+            this.e.Click += new System.EventHandler(this.Addresslabel_Click);
+            // 
+            // ProcedureBoxtxt
+            // 
+            this.ProcedureBoxtxt.BackColor = System.Drawing.Color.SkyBlue;
+            this.ProcedureBoxtxt.Location = new System.Drawing.Point(516, 76);
+            this.ProcedureBoxtxt.Name = "ProcedureBoxtxt";
+            this.ProcedureBoxtxt.Size = new System.Drawing.Size(222, 34);
+            this.ProcedureBoxtxt.TabIndex = 40;
+            this.ProcedureBoxtxt.TextChanged += new System.EventHandler(this.ProcedureBoxtxt_TextChanged);
+            // 
+            // AddressBoxtxt
+            // 
+            this.AddressBoxtxt.BackColor = System.Drawing.Color.SkyBlue;
+            this.AddressBoxtxt.Location = new System.Drawing.Point(209, 74);
+            this.AddressBoxtxt.Name = "AddressBoxtxt";
+            this.AddressBoxtxt.Size = new System.Drawing.Size(301, 34);
+            this.AddressBoxtxt.TabIndex = 41;
+            this.AddressBoxtxt.TextChanged += new System.EventHandler(this.AddressBoxtxt_TextChanged);
+            // 
+            // Register
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(750, 202);
+            this.Controls.Add(this.AddressBoxtxt);
+            this.Controls.Add(this.ProcedureBoxtxt);
+            this.Controls.Add(this.e);
+            this.Controls.Add(this.DoBBoxtxt);
+            this.Controls.Add(this.Procedurelabel);
+            this.Controls.Add(this.Doblabel);
+            this.Controls.Add(this.LogoLabel);
+            this.Controls.Add(this.PswBoxtxt);
+            this.Controls.Add(this.UserupdaterBotton);
+            this.Controls.Add(this.PswBoxlabel);
+            this.Controls.Add(this.UserBoxtxt);
+            this.Controls.Add(this.UserBoxLabel);
+            this.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.Name = "Register";
+            this.Text = "Register";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LogoLabel;
+        private System.Windows.Forms.TextBox PswBoxtxt;
+        private System.Windows.Forms.Button UserupdaterBotton;
+        private System.Windows.Forms.Label PswBoxlabel;
+        private System.Windows.Forms.TextBox UserBoxtxt;
+        private System.Windows.Forms.Label UserBoxLabel;
+        private System.Windows.Forms.Label Doblabel;
+        private System.Windows.Forms.Label Procedurelabel;
+        private System.Windows.Forms.TextBox DoBBoxtxt;
+        private System.Windows.Forms.Label e;
+        private System.Windows.Forms.TextBox ProcedureBoxtxt;
+        private System.Windows.Forms.TextBox AddressBoxtxt;
+    }
+}
